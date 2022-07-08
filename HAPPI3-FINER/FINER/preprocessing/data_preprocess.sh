2fec5c63dce90c7b5553f5fa81e538abd26cd0da
Hao <hchen069@ucr.edu>
2022-07-08 14:37:16 -0700

    data preprocess

--- HAPPI3-FINER/FINER/preprocessing/data_preprocess.sh
+++ HAPPI3-FINER/FINER/preprocessing/data_preprocess.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-python generate_go_hierarchy.py data
-python seq_dataset_human.py data major
-python seq_dataset_human.py data brain
-python domain_dataset.py data major
-python domain_dataset.py data brain
-python construct_coexp_net.py data major
-python construct_coexp_net.py data brain
+python3.7 generate_go_hierarchy.py data
+python3.7 seq_dataset_human.py data major
+python3.7 seq_dataset_human.py data brain
+python3.7 domain_dataset.py data major
+python3.7 domain_dataset.py data brain
+python3.7 construct_coexp_net.py data major
+python3.7 construct_coexp_net.py data brain
